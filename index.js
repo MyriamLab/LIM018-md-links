931d41400a6b145301df4e259d7694358fd7675c
Myriam <myriam.rivera.santiago@gmail.com>
2022-08-23 00:23:46 -0500

    se agrega comentarios descriptivos de las funciones

--- index.js
+++ index.js
@@ -5,39 +5,40 @@ module.exports = () => {
 // Accedo al módulo node:path para trabajar con rutas de archivos y directorios  
 const fs = require("node:fs");
 const { isAbsolute, resolve, extname } = require('node:path');
-
 //path es la ubicación del archivo 
 //options es un objeto con las propiedades: validate y stats
-
 function mdLinks (path, options){
-let absolutePath = path; // guardo la propiedad path en la variable absolutePath 
-if (!isAbsolute(path)){ //  si path no es absoluta, conviértela en absoluta
+let absolutePath = path; // 1.guardo la propiedad path en la variable absolutePath 
+if (!isAbsolute(path)){ //  2.si path no es absoluta, lo convierto en absoluta
   absolutePath = resolve(path) // llamo a la función resolve(path) es el path que convierto de relativo a absoluto
   console.log("es abosluto")
 }
 console.log(absolutePath) //  me devuelve en consola la ruta absoluta desde el C
 
-//verificar si el path absoluto existe
-if (!pathExits(absolutePath)){
+
+if (!pathExits(absolutePath)){  //  3.verificar si el path absoluto existe
   throw Error("El path indicado no existe");
 }
 
-const stats = fs.statSync(absolutePath);
-if (stats.isDirectory()){
-  console.log("Es un directorio")
-}else{
+//fs.statSync retorna inf. sincrónica de la ruta del archivo dado.
+const stats = fs.statSync(absolutePath); 
+//console.log(stats); // me devuelve un objeto con detalles de la ruta.
+if (stats.isDirectory()){  //  4.verifico si path es archivo o directorio.
+  console.log("Es un directorio") 
+}else{  
   console.log("Es un archivo")
-  if(!mdExtension(absolutePath)){
+  if(!mdExtension(absolutePath)){ // 5. si es archivo verifico si no es extensión .md
     throw Error("El archivo indicado no tiene la extensión .md");
   }
 }
+getLinks(absolutePath); //  6. leo el archivo
 
-getLinks(absolutePath);
 //const fileStats = fs.statSync(absolutePath);
 //console.log(fileStats);
 //else {
 // console.log("no es absoluto")}
-}
+} //fin mdLinks
+
 
 function pathExits (path) {
   try{
@@ -47,33 +48,31 @@ function pathExits (path) {
     return false;
   }
 }
-
 //const extName = path.extname('index.html');
 //console.log(extName)
-
 function mdExtension (path){
-  const extName = extname(path);
+  const extName = extname(path); // path.extname() obtengo la extensión de la ruta del archivo
   console.log(extName)
   
-  if( extName ==".md"){
+  if(extName == ".md"){
     return true;
-   
   }else {
     return false;    
   }
 }
 
 function getLinks (path){
-  const content = fs.readFileSync(path, {encoding: "utf-8"});
-  const expression = /((https?)(:\/\/)(www\.)?)?([aA-zZ,\., 0-9]+)(\.)([aA-zZ]{2,3})(((\/)([aA-zZ,0-9]+))+(.)([aZ-zZ,0-9]+))?/g;
+  const content = fs.readFileSync(path, {encoding: "utf-8"}); // sin utf-8 me devuelve el buffer
+  //console.log(content) // me devuelve todo el contenido de prueba.md
+  const expression = /((https?)(:\/\/)(www\.)?)?([aA-zZ,\., 0-9]+)(\.)([aA-zZ]{2,3})(((\/)([aA-zZ,0-9]+))+(.)([aZ-zZ,0-9]+))?/g; 
   const links = content.match(expression)
   console.log(links)
 }
 
-
 function main () {
     console.log(path)
 }
+
 
 mdLinks("C:/LABORATORIA/2. PROYECTOS/NIVEL 4/prueba.md");
 //mdLinks("C:/LABORATORIA/2. PROYECTOS/NIVEL 4/MD-LIKN-FLUJO.mdj");
